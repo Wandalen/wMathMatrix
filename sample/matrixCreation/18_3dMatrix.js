0c0d3ed6ac29f36d290517c90c41ac15b41d3dc5
Wandalen <wandalen.me@gmail.com>
2020-04-16 15:13:46 +0300

    doc : editing MatrixCreating

--- sample/matrixCreation/18_3dMatrix.js
+++ sample/matrixCreation/18_3dMatrix.js
@@ -1,13 +1,13 @@
 let _ = require( 'wmathmatrix' );
 
-var matrix3D = _.Matrix.Make( [ 2, 3, 2 ] ).copy
+var matrix3d = _.Matrix.Make( [ 2, 3, 2 ] ).copy
 ([
   1,  2,  3,
   4,  5,  6,
   7,  8,  9,
   10, 11, 12,
 ]);
-console.log( `3D matrix :\n${ matrix3D.toStr() }` );
+console.log( `3D matrix :\n${ matrix3d.toStr() }` );
 /* log : 3D matrix :
 Matrix-0
 +1 +2 +3
