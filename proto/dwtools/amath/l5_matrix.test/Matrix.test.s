2266a83d9c8e0e51b51fcf597d25954dfcff5222
Dmytro <dm.vict.kr@gmail.com>
2020-04-21 16:30:30 +0300

    Implement test routine `MakeSquare`

--- proto/dwtools/amath/l5_matrix.test/Matrix.test.s
+++ proto/dwtools/amath/l5_matrix.test/Matrix.test.s
@@ -1781,6 +1781,77 @@ function MakeZeroChangeDimsLength( test )
 
 //
 
+function MakeSquare( test )
+{
+  test.case = 'buffer - 0';
+  var got = _.Matrix.MakeSquare( 0 );
+  test.identical( got.length, 0 );
+  test.identical( got.buffer, _.longDescriptor.make( 0 ) );
+  test.identical( got.dims, [ 0, 0 ] );
+  test.identical( got.strides, null );
+  test.identical( got.stridesEffective, [ 1, 0 ] );
+
+  test.case = 'buffer - 1';
+  var got = _.Matrix.MakeSquare( 1 );
+  test.identical( got.length, 1 );
+  test.identical( got.buffer, _.longDescriptor.make( 1 ) );
+  test.identical( got.dims, [ 1, 1 ] );
+  test.identical( got.strides, null );
+  test.identical( got.stridesEffective, [ 1, 1 ] );
+
+  test.case = 'buffer - 2';
+  var got = _.Matrix.MakeSquare( 2 );
+  test.identical( got.length, 2 );
+  test.identical( got.buffer, _.longDescriptor.make( 4 ) );
+  test.identical( got.dims, [ 2, 2 ] );
+  test.identical( got.strides, null );
+  test.identical( got.stridesEffective, [ 1, 2 ] );
+
+  test.case = 'buffer - Infinity';
+  var got = _.Matrix.MakeSquare( Infinity );
+  test.identical( got.length, 1 );
+  test.identical( got.buffer, _.longDescriptor.make( 1 ) );
+  test.identical( got.dims, [ Infinity, Infinity ] );
+  test.identical( got.strides, null );
+  test.identical( got.stridesEffective, [ 0, 0 ] );
+
+  test.case = 'buffer - []';
+  var got = _.Matrix.MakeSquare
+  ([
+  ]);
+  test.identical( got.length, 0 );
+  test.identical( got.buffer, _.longDescriptor.make( 0 ) );
+  test.identical( got.dims, [ 0, 0 ] );
+  test.identical( got.strides, null );
+  test.identical( got.stridesEffective, [ 1, 0 ] );
+
+  test.case = 'buffer - [ 1 ]';
+  var got = _.Matrix.MakeSquare
+  ([
+    1
+  ]);
+  test.identical( got.length, 1 );
+  test.identical( got.buffer, _.longDescriptor.make([ 1 ]) );
+  test.identical( got.dims, [ 1, 1 ] );
+  test.identical( got.strides, null );
+  test.identical( got.stridesEffective, [ 1, 1 ] );
+
+  test.case = 'buffer - [ 1, -2, 3, -4, 5, 6, 0, 0, 2 ]';
+  var got = _.Matrix.MakeSquare
+  ([
+     1, -2,  3,
+    -4,  5,  6,
+     0,  0,  2
+  ]);
+  test.identical( got.length, 3 );
+  test.identical( got.buffer, _.longDescriptor.make([ 1, -2, 3, -4, 5, 6, 0, 0, 2 ]) );
+  test.identical( got.dims, [ 3, 3 ] );
+  test.identical( got.strides, null );
+  test.identical( got.stridesEffective, [ 3, 1 ] );
+}
+
+//
+
 function make( test )
 {
   let context = this;
@@ -14060,6 +14131,7 @@ var Self =
 
     MakeChangeDimsLength,
     MakeZeroChangeDimsLength,
+    MakeSquare,
 
     make,
     makeHelper,
