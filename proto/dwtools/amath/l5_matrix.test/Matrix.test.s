76274f64eb5b9ee5dd0d36851ea4e385d2d0df8c
Dmytro <dm.vict.kr@gmail.com>
2020-04-23 07:45:46 +0300

    Extend test routine `Make` by test case with vector with strides

--- proto/dwtools/amath/l5_matrix.test/Matrix.test.s
+++ proto/dwtools/amath/l5_matrix.test/Matrix.test.s
@@ -1562,6 +1562,14 @@ function MakeChangeDimsType( test )
   test.identical( got.strides, null );
   test.identical( got.stridesEffective, [ 1, 3 ] );
 
+  test.case = 'dims - VectorAdapter, lrange and stride';
+  var got = _.Matrix.Make( _.vectorAdapter.fromLongLrangeAndStride( [ 1, 2, 3, 1, 2 ], 2, 2, 2 ) );
+  test.identical( got.length, 2 );
+  test.identical( got.buffer, _.longDescriptor.make( 6 ) );
+  test.identical( got.dims, [ 3, 2 ] );
+  test.identical( got.strides, null );
+  test.identical( got.stridesEffective, [ 1, 3 ] );
+
   /* - */
 
   if( !Config.debug )
@@ -1582,6 +1590,77 @@ function MakeChangeDimsType( test )
 
   test.case = 'negative value in dims';
   test.shouldThrowErrorSync( () => _.Matrix.Make([ -1, 2 ]) );
+}
+
+//
+
+function MakeSquareChangeBufferLength( test )
+{
+  test.case = 'buffer - 0';
+  var got = _.Matrix.MakeSquare( 0 );
+  test.identical( got.length, 0 );
+  test.identical( got.buffer, _.longDescriptor.make( 0 ) );
+  test.identical( got.dims, [ 0, 0 ] );
+  test.identical( got.strides, null );
+  test.identical( got.stridesEffective, [ 1, 0 ] );
+
+  test.case = 'buffer - 1';
+  var got = _.Matrix.MakeSquare( 1 );
+  test.identical( got.length, 1 );
+  test.identical( got.buffer, _.longDescriptor.make( 1 ) );
+  test.identical( got.dims, [ 1, 1 ] );
+  test.identical( got.strides, null );
+  test.identical( got.stridesEffective, [ 1, 1 ] );
+
+  test.case = 'buffer - 2';
+  var got = _.Matrix.MakeSquare( 2 );
+  test.identical( got.length, 2 );
+  test.identical( got.buffer, _.longDescriptor.make( 4 ) );
+  test.identical( got.dims, [ 2, 2 ] );
+  test.identical( got.strides, null );
+  test.identical( got.stridesEffective, [ 1, 2 ] );
+
+  test.case = 'buffer - Infinity';
+  var got = _.Matrix.MakeSquare( Infinity );
+  test.identical( got.length, 1 );
+  test.identical( got.buffer, _.longDescriptor.make( 1 ) );
+  test.identical( got.dims, [ Infinity, Infinity ] );
+  test.identical( got.strides, null );
+  test.identical( got.stridesEffective, [ 0, 0 ] );
+
+  test.case = 'buffer - []';
+  var got = _.Matrix.MakeSquare
+  ([
+  ]);
+  test.identical( got.length, 0 );
+  test.identical( got.buffer, _.longDescriptor.make( 0 ) );
+  test.identical( got.dims, [ 0, 0 ] );
+  test.identical( got.strides, null );
+  test.identical( got.stridesEffective, [ 1, 0 ] );
+
+  test.case = 'buffer - [ 1 ]';
+  var got = _.Matrix.MakeSquare
+  ([
+    1
+  ]);
+  test.identical( got.length, 1 );
+  test.identical( got.buffer, _.longDescriptor.make([ 1 ]) );
+  test.identical( got.dims, [ 1, 1 ] );
+  test.identical( got.strides, null );
+  test.identical( got.stridesEffective, [ 1, 1 ] );
+
+  test.case = 'buffer - [ 1, -2, 3, -4, 5, 6, 0, 0, 2 ]';
+  var got = _.Matrix.MakeSquare
+  ([
+     1, -2,  3,
+    -4,  5,  6,
+     0,  0,  2
+  ]);
+  test.identical( got.length, 3 );
+  test.identical( got.buffer, _.longDescriptor.make([ 1, -2, 3, -4, 5, 6, 0, 0, 2 ]) );
+  test.identical( got.dims, [ 3, 3 ] );
+  test.identical( got.strides, null );
+  test.identical( got.stridesEffective, [ 3, 1 ] );
 }
 
 //
@@ -1947,6 +2026,14 @@ function MakeZeroChangeDimsType( test )
 
   test.case = 'dims - VectorAdapter';
   var got = _.Matrix.MakeZero( _.vectorAdapter.from([ 3, 2 ]) );
+  test.identical( got.length, 2 );
+  test.identical( got.buffer, _.longDescriptor.make( 6 ) );
+  test.identical( got.dims, [ 3, 2 ] );
+  test.identical( got.strides, null );
+  test.identical( got.stridesEffective, [ 1, 3 ] );
+
+  test.case = 'dims - VectorAdapter, lrange and stride';
+  var got = _.Matrix.MakeZero( _.vectorAdapter.fromLongLrangeAndStride( [ 1, 2, 3, 1, 2 ], 2, 2, 2 ) );
   test.identical( got.length, 2 );
   test.identical( got.buffer, _.longDescriptor.make( 6 ) );
   test.identical( got.dims, [ 3, 2 ] );
@@ -14256,7 +14343,7 @@ var Self =
 
     MakeChangeDimsLength,
     MakeChangeDimsType,
-    MakeSquare,
+    MakeSquareChangeBufferLength,
     MakeZeroChangeDimsLength,
     MakeZeroChangeDimsType,
 
